4a211f231381c8af3465ce4b88a14623f02e55ae
ackepenek <ahmetcan196@gmail.com>
2015-12-12 16:43:56 +0000

    proje eklendi

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.filter(yaratilis_tarihi__lte=timezone.now()).order_by('yaratilis_tarihi')
+    return render(request, 'blog/post_list.html', {'posts': posts})
